264f4d8be537a82510690ab409e2006ca7465f39
Darien Bernal <darien.bernal@gmail.com>
2025-02-15 23:00:36 -0500

    Converted BatSignal solution to a promise

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Async Programming Practice
+// Practicing with Batman terminology in order to understand concepts with a different context
 
-// The Bat-Signal Delay
+// 1. The Bat-Signal Delay
 // The Bat-Signal has been activated, but Batman is in the middle of analyzing crime reports. The Batcomputer must alert him as soon as possible, but without interrupting his current task.
 
 function analyzeReports() {
@@ -9,10 +10,30 @@ function analyzeReports() {
 
 function alertBatSignal(){
     setTimeout (() => {
-        console.log("Bat-Signal has been activated: Batman is needed immaediately!")
+        console.log("Bat-Signal has been activated: Batman is needed immediately!")
+        // Three-second delay
     }, 3000);
 }
 
 analyzeReports();
 alertBatSignal();
+// Executes before the alert because synchronous code is executed first
 console.log("Batman's evening continues...");
+
+console.log("------");
+
+// Convert solution to a promise
+function promiseBatSignalAlert() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Bat-Signal has been activated: Batman is the only one who can save Gotham!");
+        }, 5000);
+    })
+}
+
+analyzeReports();
+promiseBatSignalAlert().then((message) => {
+    console.log(message);
+})
+
+console.log("Batman is focusing on his work, as usual...")
