2be07393946616204c4628fe63953c823b025bf0
Darien Bernal <darien.bernal@gmail.com>
2025-02-16 23:57:10 -0500

    Added function that returns Penguin location when Promise is fulfilled

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function analyzeReports() {
     console.log("Batman is anazlying crime reports for cases.")
 }
 
-function alertBatSignal(){
-    setTimeout (() => {
+function alertBatSignal() {
+    setTimeout(() => {
         console.log("Bat-Signal has been activated: Batman is needed immediately!")
         // Three-second delay
     }, 3000);
@@ -18,6 +18,7 @@ function alertBatSignal(){
 analyzeReports();
 alertBatSignal();
 // Executes before the alert because synchronous code is executed first
+// Event Loop: Immediate tasks are first, then async tasks
 console.log("Batman's evening continues...");
 
 console.log("------");
@@ -37,3 +38,22 @@ promiseBatSignalAlert().then((message) => {
 })
 
 console.log("Batman is focusing on his work, as usual...")
+
+// 2. Robin's Recon Mission
+// Batman sends Robin to gather intel on Penguin’s smuggling operation. However, the mission has three possible outcomes:
+
+// - Robin successfully reports back with Penguin’s location.
+// - Robin encounters trouble and cannot complete the mission.
+// - Robin gets captured, and Batman must step in.
+
+function investigatePenguin() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Penguin is located at the Iceberg Lounge!");
+        },);
+    })
+}
+
+investigatePenguin().then((message) => {
+    console.log(message);
+})
